9ad9ad3ddb259027e73808eddcfcc0bc8d8bc7ab
chriskasparmayo <kaspar.christopher@mayo.edu>
2019-12-30 22:13:31 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project-name" {
+  default = "iacdemo"
+}
+
+variable "stackname" {
+  default = "gsoc-172407"
+}
+
+variable "location" {
+  default = "US"
+}
+
+variable "storage-class" {
+  default = "REGIONAL"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
